4ec7febb7ae47cae2bf8d9a64f93a683441dfe2a
noklash <123834419+noklash@users.noreply.github.com>
2023-08-18 11:45:28 +0300

    added role based authorization to client and extra routes

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,10 +1,20 @@
 import type { NextAuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
-import CredentialsProvider from 'next-auth/providers/credentials'
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { GithubProfile } from 'next-auth/providers/github';
 
 export const options: NextAuthOptions = {
     providers: [
         GitHubProvider({
+            profile(profile: GithubProfile){
+                // console.log(profile)
+                return {
+                    ...profile,
+                    role: profile.role ?? "user",
+                    id: profile.id.toString(),
+                    image: profile.avatar_url,
+                }
+            },
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
         }),
@@ -26,7 +36,7 @@ export const options: NextAuthOptions = {
                 // This is where you need to retrieve user data 
                 // to verify with credentials
                 // Docs: https://next-auth.js.org/configuration/providers/credentials
-                const user = { id: "42", name: "Dave", password: "nextauth" }
+                const user = { id: "42", name: "Noklash", password: "noklashn", role: "manager" }
 
                 if (credentials?.username === user.name && credentials?.password === user.password) {
                     return user
@@ -36,4 +46,15 @@ export const options: NextAuthOptions = {
             }
         })
     ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) token.role = user.role
+            return token 
+        }, 
+        // IF YOU WANT TO USE THE ROLE IN CLIENT COMPONENTS
+        async session({ session, token}) {
+            if (session?.user) session.user.role = token.role
+            return session 
+        }
+    }
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,43 @@
 // Without a defined matcher, this one line applies next-auth 
 // to the entire project
-export { default } from "next-auth/middleware"
+// export { default } from "next-auth/middleware"
+// we no longer need code above
+
+// Ref: https://next-auth.js.org/configuration/nextjs#advanced-usage
+
+import { withAuth, NextRequestWithAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
+
+export default withAuth(
+    //  withAuth auguments your "Request" with the user's token.
+    function middleware(request: NextRequestWithAuth){
+        console.log(request.nextUrl.pathname)
+        console.log(request.nextauth.token)
+
+        if (request.nextUrl.pathname.startsWith("/extra")
+        && request.nextauth.token?.role !== "admin"){
+          return NextResponse.rewrite(
+            new URL("/denied", request.url)
+          )  
+     }
+
+    //  the above if statement takes the user to the Denied page if the role is not an admin
+
+    if (request.nextUrl.pathname.startsWith("/client")
+        && request.nextauth.token?.role !== "admin"
+        && request.nextauth.token?.role !== "manager"){
+            return NextResponse.rewrite(
+                new URL("/denied", request.url)
+              ) 
+        }
+    }, 
+    {
+        callbacks: {
+            authorized: ({ token }) => !!token //just to verify that there's a token
+        },
+    }
+)
 
 // Applies next-auth only to matching routes - can be regex
 // Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-export const config = { matcher: ["/extra", "/dashboard"] }
+export const config = { matcher: ["/extra", "/client", "/dashboard" ] }
